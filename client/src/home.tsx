19a4eb413b236c84111d1163e124ef7dda4d5a67
Jeff <lim_jeff@hotmail.fr>
2024-09-23 16:29:20 +0200

    Remove Game

--- client/src/home.tsx
+++ client/src/home.tsx
@@ -29,21 +29,17 @@ export const Home = () => {
       <div className="home-container">
         <section className='news'>
           <h2 className='home-title__news'>News</h2>
-          <div className="home-container__filter-button-box">
+          {    /* <div className="home-container__filter-button-box">
             <button onClick={() => handleFilterNewsArray('All')}
               disabled={!(news.length > 0)}>
               All
-            </button>
-            <button onClick={() => handleFilterNewsArray('Game')}
-              disabled={!(news.filter(object => object.subject === 'Game').length > 0)}>
-              Game
             </button>
             <button onClick={() => handleFilterNewsArray('Collection')}
               disabled={!(news.filter(object => object.subject === 'Collection').length > 0)}
             >
               Collection
             </button>
-          </div>
+          </div>*/}
           <Pagination sizePage={sizePage}
             data={newsArray} />
         </section>
