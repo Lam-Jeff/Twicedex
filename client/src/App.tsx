017b3b56aac8e8853deb15a73870706900cd3a3a
Jeff <lim_jeff@hotmail.fr>
2024-11-11 19:32:27 +0100

    Remove game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,13 @@ import { Home } from './home';
 import { About } from './about';
 import { ScrollUpButton } from './scrollUpButton';
 import { PageNotFound } from './pageNotFound';
-import { MiniGame } from './miniGame';
+import { Login } from './login';
 import { ScrollToTop } from './scrollToTop';
 import { Collection } from './collection';
 import { Settings } from './settings';
-
+import { AuthProvider } from './authProvider';
+import { Profile } from './profile';
+import { ProtectedRoute } from './protectedRoute';
 interface IApplicationProps { }
 
 const App: React.FunctionComponent<IApplicationProps> = () => {
@@ -17,21 +19,29 @@ const App: React.FunctionComponent<IApplicationProps> = () => {
     <div className="App">
       <BrowserRouter>
         <ScrollToTop />
-        <Header />
-        <Routes>
-          <Route path="/" >
-            <Route index element={<Home />} />
-          </Route>
-          {/*<Route path="/minigame" element={<MiniGame />} />*/}
-          <Route path="/about" element={<About />} />
-          <Route path="/collection/:categoryParam?/:eraParam?" >
-            <Route index element={<Collection />} />
-            <Route path='settings' element={<Settings />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
+        <AuthProvider>
+          <Header />
+          <Routes>
+            <Route path="/" >
+              <Route index element={<Home />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" >
+              <Route index element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+              <Route path='settings' element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+            </Route>
+            <Route path="/about" element={<About />} />
+            <Route path="/collection/:categoryParam?/:eraParam?" >
+              <Route index element={<Collection />} />
+              <Route path='settings' element={<Settings />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
 
-        </Routes>
+          </Routes>
+        </AuthProvider>
         <ScrollUpButton />
+
+
       </BrowserRouter>
     </div>
   );
--- client/src/helpers.tsx
+++ client/src/helpers.tsx
@@ -1,10 +1,8 @@
-import game from './files/game.json'
-import albums from './files/albums.json'
-import { ICardsProps } from './collection'
-import { IStatsProps } from './miniGame'
+import albums from './files/albums.json';
+import { ICardsProps } from './collection';
+import { isThresholdAchievementsReached, isThresholdCategoryReached, isThresholdCollectionReached, isThresholdEraReached, isThresholdMembersReached, isUserCreated } from './predicates';
+import { TAchievementProps } from './types/achievement';
 
-const now = new Date()
-export const imageOfTheDay = game[(now.getFullYear() * now.getDate() * (now.getMonth() + 1)) % game.length]
 export const firstElementOfCategory: Record<string, number> = {
     'Korean Albums': albums.findIndex(object => object.categories.includes('Korean Albums')),
     'Japanese Albums': albums.findIndex(object => object.categories.includes('Japanese Albums')),
@@ -67,58 +65,12 @@ export function getValuesArrayFromUrl(urlParams: string | null, array: { name: s
  * 
  * @returns {string[]}
  */
-export function filterValues(data: { name: string, checked: boolean }[]) :  string[]{
+export function filterValues(data: { name: string, checked: boolean }[]): string[] {
     const filteredData = data.filter(object => {
         return object.checked
     }).map(object => object.name)
 
     return filteredData
-}
-
-/* MINIGAME */
-
-/**
- * Find longest streak and current streak in an array of stats.
- * 
- * @param {IStatsProps[]} array - an array with user's stats
- * 
- * @returns {{ longestStreak: number, currentStreak: number } }
- */
-export function findWinStreak(array: IStatsProps[]): { longestStreak: number, currentStreak: number } {
-    let longestStreak = 0;
-    let streakCount = 0;
-    let streakList = []
-    let currentStreak = 0;
-
-    if (array.length === 1) {
-        longestStreak = array[0].findAnswer ? 1 : 0
-        currentStreak = array[0].findAnswer ? 1 : 0
-        return { longestStreak, currentStreak }
-    } else if (array.length === 0) { return { longestStreak, currentStreak } }
-
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].findAnswer) {
-            streakCount++;
-            if (i === array.length - 1) {
-                if (streakCount > longestStreak) {
-                    longestStreak = streakCount;
-                }
-                streakList.push(streakCount)
-            }
-        } else {
-            if (streakCount > longestStreak) {
-                longestStreak = streakCount;
-            }
-            if (i !== array.length - 1) {
-                streakList.push(streakCount)
-                streakCount = 0;
-            } else {
-                streakList.push(0)
-            }
-        }
-    }
-    currentStreak = streakList[streakList.length - 1]
-    return { longestStreak, currentStreak };
 }
 
 /* COLLECTION */
@@ -178,3 +130,129 @@ export function getPages<T>(array: T[], numberElementsPerPage: number): number[]
     return pages;
 }
 
+/* PROFILE */
+
+/**
+ * Get progression by era.
+ * 
+ * @param {number[]} userCollection - an array representing the cards acquired
+ * @param {ICardsProps[]} cards - list of cards in the database
+ * 
+ * @returns {Record<string, number>}
+ */
+export function computeProgressionPercentByEra(userCollection: number[], cards: ICardsProps[]): Record<string, number> {
+    let acquiredList: Record<string, number> = {};
+    let totalList: Record<string, number> = {};
+    let progression: Record<string, number> = {};
+
+    cards.forEach(card => totalList[card.era] = (totalList[card.era] || 0) + 1);
+    userCollection.forEach((value) => {
+        let _index = cards.find(card => card.id === value)?.era;
+        if (_index) acquiredList[_index] = (acquiredList[_index] || 0) + 1;
+    });
+
+    for (const key in totalList) {
+        if (acquiredList[key]) progression[key] = Math.round(acquiredList[key] / totalList[key] * 100);
+        else progression[key] = 0;
+    };
+    return progression;
+}
+
+/**
+ * Get progression by category.
+ * 
+ * @param {number[]} userCollection - an array representing the cards acquired
+ * @param {ICardsProps[]} cards - list of cards in the database
+ * 
+ * @returns {Record<string, { percent: number, acquired: number, total: number }>}
+ */
+export function computeProgressionPercentByCategory(userCollection: number[], cards: ICardsProps[]): Record<string, { percent: number, acquired: number, total: number }> {
+    let acquiredList: Record<string, number> = {}
+    let totalList: Record<string, number> = {}
+    let progression: Record<string, { percent: number, acquired: number, total: number }> = {}
+
+    for (let _cat in firstElementOfCategory) {
+        acquiredList[_cat] = 0;
+        totalList[_cat] = 0;
+    }
+
+    cards.forEach((value) => {
+        let _categories = value.categories
+        if (userCollection.includes(value.id)) {
+            _categories.forEach(cat => { acquiredList[cat] += 1; })
+        }
+        _categories.forEach(cat => totalList[cat] += 1)
+    })
+
+    for (let _cat in firstElementOfCategory) {
+        progression[_cat] = { percent: Math.round(acquiredList[_cat] / totalList[_cat] * 100), acquired: acquiredList[_cat], total: totalList[_cat] }
+    }
+    return progression;
+}
+
+/**
+ * Get progression by members.
+ * 
+ * @param {ICardsProps[]} cards - list of cards in the database
+ * @param {number[]} userCollection - an array representing the cards acquired
+ * @param {string[]} members - an array representing the members
+
+ * 
+ * @returns {number}
+ */
+export function computeProgressionByMembers(userCollection: number[], cards: ICardsProps[], members: string[]): number {
+    let collectionFiltered: ICardsProps[] = []
+    if(members.length== 0) return 0;
+    if (members.length == 1) {
+        let member = members[0];
+        collectionFiltered = cards.filter(card => userCollection.includes(card.id) && card.members.length == 1 && card.members.includes(member));
+    } else {
+        collectionFiltered = cards.filter(card => userCollection.includes(card.id) && members.every(member => card.members.includes(member)));
+    }
+    return collectionFiltered.length;
+}
+
+/**
+ * Check whether achievement is successful.
+ * 
+ * @param {TAchievementProps} achievement - an array representing the cards acquired
+ * @param {number[]} userAchievements - an array representing the achievements acquired
+ * @param {ICardsProps[]} cards - list of cards in the database
+ * @param {number[]} userCollection - an array representing the cards acquired
+ * @param {Record<string, number>} percentEras - an array representing the progression of all eras
+ * @param {Record<string, { percent: number, acquired: number, total: number }>} percentCategories - an array representing the progression of all categories
+ * 
+ * @returns {boolean}
+ */
+export function checkAchievement(achievement: TAchievementProps, userAchievements: number[], cards: ICardsProps[], userCollection: number[], percentEras: Record<string, number>, percentCategories: Record<string, { percent: number, acquired: number, total: number }>): boolean {
+    let criteria = achievement.criteria;
+    let rule = criteria.trigger.rule;
+    let conditions = criteria.conditions;
+    let target = criteria.trigger.target ? criteria.trigger.target : [];
+    let threshold = criteria.trigger.match;
+    if (conditions) {
+        conditions.forEach(condition => {
+            if (!userAchievements.includes(condition)) return false;
+        });
+    }
+
+    switch (rule) {
+        case "user_created":
+            return isUserCreated();
+        case "threshold_era":
+            let era = target[0];
+            return isThresholdEraReached(threshold, percentEras[era]);
+        case "threshold_category":
+            let category = target[0];
+            return isThresholdCategoryReached(threshold, percentCategories[category].percent);
+        case "threshold_members":
+            let numberCardsAcquired = computeProgressionByMembers(userCollection, cards, target);
+            return isThresholdMembersReached(threshold, numberCardsAcquired);
+        case "threshold_collection_size":
+            return isThresholdCollectionReached(threshold, userCollection);
+        case "threshold_achievements":
+            return isThresholdAchievementsReached(threshold, userAchievements);
+        default:
+            return false;
+    }
+}
